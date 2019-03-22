7cf4448d3ceaea18c11884ea7b29150cc784299d
Lue <lue_hang@hotmail.com>
2019-03-21 23:29:26 -0500

    Added info for mixin.

--- src/TimerMixin.js
+++ src/TimerMixin.js
@@ -1,5 +1,35 @@
 "use strict";
 
+/*
+TimerMixin provides timer functions for executing code in
+the future that are safely cleaned up when the component unmounts
+
+Using bare setTimeout, setInterval, setImmediate and requestAnimationFrame calls
+is very dangerous because if you forget to cancel the request before the
+component is unmounted, you risk the callback throwing an exception.
+
+If you include TimerMixin, then you can replace your calls to
+`setTimeout(fn, 500)` with `this.setTimeout(fn, 500)` (just prepend `this.`) and
+everything will be properly cleaned up for you.
+
+## Example
+
+```js
+var React = require("react");
+var TimerMixin = require("react-timer-mixin");
+
+var Component = React.createClass({
+  mixins: [TimerMixin],
+  componentDidMount() {
+    this.setTimeout(
+      () => { console.log("I do not leak!"); },
+      500
+    );
+  }
+});
+```
+*/
+
 var GLOBAL = typeof window === "undefined" ? global : window;
 
 var setter = function(_setter, _clearer, array) {
